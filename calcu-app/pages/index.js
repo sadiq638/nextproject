9ca3680beb55046c7013ff4c5957feeada69e835
sadiq638 <msadik010@gmail.com>
2022-11-24 20:22:57 +0530

    new changes in index.js

--- calcu-app/pages/index.js
+++ calcu-app/pages/index.js
@@ -1,7 +1,22 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+function Buton(props){
+  
+  
+  return(
+    <>
+    <button className={styles["button"]+' '+styles["button:active"]}></button>
+    <button className={styles["button"]+' '+styles["button:active"]}></button>
+    <button className={styles["button"]+' '+styles["button:active"]} ></button>
+    <button className={styles["button"]+' '+styles["button:active"]}></button>
+    </>
+  )
+}
+
+ function Home() {
+  
+  
   return (
     <div>
       <Head>
@@ -13,31 +28,21 @@ export default function Home() {
       <main className={styles.main}>
       <div className={styles["div"]+'  '+styles["c"]} id="result">0</div>
             
-            <div className={styles.div}>
-            <button className={styles["button"]+' '+styles["button:active"]} id="1">1</button>
-            <button className={styles["button"]+' '+styles["button:active"]} id="2">2</button>
-            <button className={styles["button"]+' '+styles["button:active"]} id="3">3</button>
-            <button className={styles["button"]+' '+styles["button:active"]} id="min">-</button>
-            </div>
+      <div className={styles.div}>
+            <Buton  />
+       </div>  
         
              <div className={styles.div}>
-            <button className={styles["button"]+' '+styles["button:active"]}  id="4">4</button>
-            <button className={styles["button"]+' '+styles["button:active"]} id="5">5</button>
-            <button  className={styles["button"]+' '+styles["button:active"]} id="6">6</button>
-            <button className={styles["button"]+' '+styles["button:active"]}  id="plus">+</button>
+             <Buton />
              </div>
 
             <div className={styles.div}>
-            <button className={styles["button"]+' '+styles["button:active"]} id="7">7</button>
-            <button className={styles["button"]+' '+styles["button:active"]}  id="8">8</button>
-            <button className={styles["button"]+' '+styles["button:active"]} id="9">9</button>
-            <button  className={styles["button"]+' '+styles["button:active"]} id="divide"> /</button>
+            <Buton />
             </div>
            <div className={styles.div}>
-            <button className={styles["button"]+' '+styles["button:active"]} id="c">c</button>
-            <button className={styles["button"]+' '+styles["button:active"]} id="ca">ca</button>
-            <button className={styles["button"]+' '+styles["button:active"]}  id="mul">*</button>
-            <button  className={styles["button"]+' ' +styles["equal"]} >=</button>
+           <Buton />
+
+
            </div> 
       </main>
 
@@ -45,3 +50,4 @@ export default function Home() {
     </div>
   )
   }
+export default Home
