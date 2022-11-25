dc18d39179738c3a07f9253ffcc29928a9ef769d
vmtn2 <sathrt@gmail.com>
2022-11-25 09:52:00 +0530

    Project folder moved

---
+++ calcu-app/tailwind.config.js
@@ -0,0 +1,12 @@
+// @type {import('tailwindcss').Config}
+module.exports = {
+  content: [
+    './calcu-app/pages/**/*.js',
+    './pages/**/*.js',
+    './components/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ components/CalculatorButton.js
@@ -0,0 +1,13 @@
+// Write a button
+
+export default function CalculatorButton(props) {
+    return (
+        <button
+        className={props.className}
+        onClick={props.onClick}
+        value={props.value}
+        >
+        {props.value}
+        </button>
+    );
+}
